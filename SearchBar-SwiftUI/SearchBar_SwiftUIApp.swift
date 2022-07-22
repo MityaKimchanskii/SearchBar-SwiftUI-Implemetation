16f3998cc4bc69c6e1c0964257c4b95f46d4f3ea
Dmitrii Kim <mitya_kim@icloud.com>
2022-07-21 22:55:28 -0700

    Initial Commit

---
+++ SearchBar-SwiftUI/SearchBar_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchBar_SwiftUIApp.swift
+//  SearchBar-SwiftUI
+//
+//  Created by Mitya Kim on 7/21/22.
+//
+
+import SwiftUI
+
+@main
+struct SearchBar_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
